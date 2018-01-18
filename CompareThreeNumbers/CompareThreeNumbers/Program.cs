9a3dcab4576b3ee95e6831451ccfa1a56bc31055
recasad <33033835+recasad@users.noreply.github.com>
2018-01-18 14:42:29 -0600

    Update Compare Numbers

---
+++ CompareThreeNumbers/CompareThreeNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberString;
+            int num1, num2, num3;
+
+            System.Console.Write("Enter an integer ");
+            numberString = System.Console.ReadLine();
+            num1 = Convert.ToInt32(numberString);
+            System.Console.Write("Enter an integer ");
+            numberString = System.Console.ReadLine();
+            num2 = Convert.ToInt32(numberString);
+            System.Console.Write("Enter an integer ");
+            numberString = System.Console.ReadLine();
+            num3 = Convert.ToInt32(numberString);
+
+            if (num1 == num2)
+                if (num1 == num3)
+                    System.Console.WriteLine("All three numbers are equal");
+                else
+                    System.Console.WriteLine("First two are equal");
+            else
+                if (num1 == num3)
+                    System.Console.WriteLine("First and last are equal");
+                else
+                    if (num2 == num3)
+                        System.Console.WriteLine("Last two are equal");
+                    else
+                        System.Console.WriteLine("No two numbers are equal");
+        }
+    }
+}
