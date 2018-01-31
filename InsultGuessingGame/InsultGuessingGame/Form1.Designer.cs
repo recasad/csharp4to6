cc878ad01cb55081e384fc428431d2eecb20695a
recasad <33033835+recasad@users.noreply.github.com>
2018-01-31 13:59:39 -0600

    Created Hawkeye And NewtonsSecondLaw

---
+++ InsultGuessingGame/InsultGuessingGame/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace InsultGuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputNumber = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInputNumber
+            // 
+            this.txtInputNumber.Location = new System.Drawing.Point(31, 48);
+            this.txtInputNumber.Name = "txtInputNumber";
+            this.txtInputNumber.Size = new System.Drawing.Size(105, 20);
+            this.txtInputNumber.TabIndex = 0;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(31, 74);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(105, 23);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Guess a number 1-20";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(31, 100);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(105, 55);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(163, 167);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtInputNumber);
+            this.Name = "Form1";
+            this.Text = "Guesser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputNumber;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ InsultGuessingGame/InsultGuessingGame/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsultGuessingGame
+{
+    public partial class Form1 : Form
+    {
+        int intGuessedNumber;
+        int intNumber = 13;
+        int intLastNumber = -1;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            intGuessedNumber = Convert.ToInt32(txtInputNumber.Text);
+
+            do
+            {
+                if (intGuessedNumber > intNumber)
+                {
+                    lblOutput.Text = ("Try a smaller number");
+                }
+                if (intGuessedNumber < intNumber)
+                {
+
+                }
+                if (intGuessedNumber == intNumber)
+                {
+                    lblOutput.Text = ("wow that's actually the right number");
+                }
+                intLastNumber = intGuessedNumber;
+
+
+            } while (intNumberGuessed == lowerNumber || intGuessNumber == higherNumber);
+        }
+    }
+}
